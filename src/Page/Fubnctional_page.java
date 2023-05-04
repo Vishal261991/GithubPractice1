b8e3c13f5f72961dfa55aa4a77bf559f9fa2bab1
Vishal261991 <desaivishal2691@gmail.com>
2023-05-05 00:29:40 +0530

    added/deleted/modified

---
+++ src/Page/Fubnctional_page.java
@@ -0,0 +1,11 @@
+package Page;
+
+public class Fubnctional_page
+{
+	public static void main(String[] args)
+	{
+		System.out.println("New page is created");
+		
+	}
+
+}
--- src/Test/Home_Page_Test.java
+++ src/Test/Home_Page_Test.java
@@ -5,6 +5,8 @@ public class Home_Page_Test
 	public void Test2()
 	{
 		System.out.println("Test2:Home Page");
+		System.out.println("add one more line");
+		
 	}
 	
 
